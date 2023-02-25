eccdbb45c9ec5e3973a255fc5142f0604dd64cd5
christian barillas <70426461+ChristianBarillas@users.noreply.github.com>
2023-02-25 10:29:32 -0600

    Agregando estilos

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cdogecoin%2Ccardano&vs_currencies=usd&include_24hr_change=true').then(res => {
-
-res.json()
-}).then(json=>{
+fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cdogecoin%2Ccardano&vs_currencies=usd&include_24hr_change=true').then(res => res.json()).then(json=>{
 
 const container = document.querySelector('.container');
 const coins = Object.getOwnPropertyNames(json);
+
+console.log(coins)
 
 for (coin of coins){
     const coinInfo = json[`${coin}`];
     const price = coinInfo.usd;
     const change = coinInfo.usd_24h_change.toFixed(5);
 
-    container.innerHTML = 
+    container.innerHTML += 
     `
-    <div class = "coin ${change < 0 ? 'falling':'rising'}>
+    <div class = "coin ${change < 0 ? 'falling':'rising'}">
     
-    <div class = "coni-log">
+    <div class = "coin-log">
     <img src = "./media/${coin}.png">
     </div>
 
@@ -38,5 +37,5 @@ for (coin of coins){
     `
 }
 
-})
+});
 
